ab49749668f55af5264bd67da86b11f12278088e
Alex Agapov <agapov.one@gmail.com>
2019-03-24 09:49:57 +0500

    Add colors

---
+++ LivePhotoToVideo/Views/Styles/UIColor+Additions.swift
@@ -0,0 +1,33 @@
+//
+//  UIColor+Additions.swift
+//  Live Photo
+//
+//  Generated on Zeplin. (3/24/2019).
+//  Copyright (c) 2019 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var darkCharcoal: UIColor {
+    return UIColor(red: 19.0 / 255.0, green: 19.0 / 255.0, blue: 26.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var charcoal: UIColor {
+    return UIColor(red: 49.0 / 255.0, green: 49.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var milkChocolate: UIColor {
+    return UIColor(red: 110.0 / 255.0, green: 72.0 / 255.0, blue: 38.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var projectRed: UIColor {
+    return UIColor(red: 235.0 / 255.0, green: 37.0 / 255.0, blue: 61.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var projectGreen: UIColor {
+    return UIColor(red: 38.0 / 255.0, green: 235.0 / 255.0, blue: 77.0 / 255.0, alpha: 1.0)
+  }
+
+}
